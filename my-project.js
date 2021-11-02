03ec1b5ed61730add7efa65491a6e5aa2d2d8c56
GianlucaP <Huntelaar11w@hotmail.it>
2021-11-02 18:24:00 +0100

    Create my-project.js

---
+++ my-project.js
@@ -0,0 +1,4 @@
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const username = urlParams.get('username')
+window.location.href = "wishlist://username:username";

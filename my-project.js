12898f6885cd4567dac84e1e595176fb2e393b14
GianlucaP <Huntelaar11w@hotmail.it>
2021-11-02 20:16:24 +0100

    Delete my-project.js

--- my-project.js
+++
@@ -1,4 +0,0 @@
-const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
-const product = urlParams.get('username')
-window.location = "wishlist://username:" + product;

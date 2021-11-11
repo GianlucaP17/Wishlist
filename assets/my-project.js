b8dec3c37158b364aa3b755aff143a2232ed47d8
GianlucaP <Huntelaar11w@hotmail.it>
2021-11-11 19:50:30 +0100

    Update my-project.js

--- assets/my-project.js
+++ assets/my-project.js
@@ -2,4 +2,4 @@ const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const username = urlParams.get('username')
 const wish = urlParams.get('wish')
-window.location = "wishlist://username:" + username + "&wish:" wish;
+window.location = "wishlist://username:" + username + "&wish:" + wish;
